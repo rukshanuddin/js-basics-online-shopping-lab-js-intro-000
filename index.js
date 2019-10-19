db2103ee9a5cd4695b4e13aee8290a9a7d8cf94f
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-10-19 17:35:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function setCart(c) {
 function addToCart(item) {
  var newItem = {itemName:item, itemPrice:Math.floor((Math.random()*100)+1)}
  cart.push(newItem)
- return "{item} has been added to your cart."// write your code here
+ return "#{item} has been added to your cart."// write your code here
 }
 
 function viewCart() {
